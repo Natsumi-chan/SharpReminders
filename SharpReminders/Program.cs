c26334e9c64b3d36a67aa36697f809eb51902936
Natsumi Himura <natsumihimura@gmail.com>
2014-11-17 22:06:02 -0500

    Initial commit
    all files

---
+++ SharpReminders/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharpReminders
+{
+    static class Program
+    {
+        public static string programName = "";
+        public static string programAuthor = "";
+        public static string programVersion = "";
+
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+        }
+    }
+}
